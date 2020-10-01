4696d33b8ca6107f1152d7c31b279d96edd38f38
mike <mike2mars@hotmail.com>
2020-10-01 17:37:48 +0200

    Add HomeController

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/home", name="home", methods="GET")
+     */
+    public function index(): RedirectResponse
+    {
+        return $this->redirectToRoute("app_events");
+    }
+}
