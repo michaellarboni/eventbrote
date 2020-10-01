0d44d2539db74c3c3d40939fc35a93b84bf0694f
mike <mike2mars@hotmail.com>
2020-10-01 17:49:40 +0200

    Add route for show function in EventsController and add template

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,5 +20,15 @@ class EventsController extends AbstractController
         $events = $repo->findAll();
 
         return $this->render('/events/index.html.twig', compact('events'));
+    }
+
+    /**
+     * @Route("/events/{id<[0-9]+>}", name="app_events_show", methods="GET")
+     * @param Event $event
+     * @return Response
+     */
+    public function show(Event $event) : Response
+    {
+        return $this->render('/events/show.html.twig', compact('event'));
     }
 }
