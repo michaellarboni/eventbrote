1546ac6d697060d5b457a6038d50540cb92e2537
mike <mike2mars@hotmail.com>
2020-10-01 11:11:58 +0200

    Add AppExtension.php, function pluralize and formatPrice

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Twig;
+
+use App\Entity\Event;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+
+class AppExtension extends AbstractExtension
+{
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('pluralize', [$this, 'pluralize']),
+            new TwigFunction('formatPrice', [$this, 'formatPrice']),
+        ];
+    }
+
+    // ?string $plural = null signifie optionel et par defaut null
+    public function pluralize(int $count, string $singular, ?string $plural = null): string
+    {
+        $plural = $plural ?? $singular . 's';
+
+        // si $count vaut 1, $string vaut $singular sinon $plural
+        $str = $count === 1 ? $singular : $plural;
+
+        return "$count $str";
+    }
+
+    public function formatPrice(Event $event): string
+    {
+        if ($event->isFree()) {
+            return 'Free!';
+        } else {
+            return $event->getPrice() . '€';
+        }
+    }
+}
