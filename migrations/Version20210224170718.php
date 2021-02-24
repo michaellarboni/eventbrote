272ebcab37d2a1622c084d31422025a01f8c4bdc
mike <mike2mars@hotmail.com>
2021-02-24 18:08:50 +0100

    Create table events

--- migrations/Version20210224170718.php
+++ migrations/Version20210224170718.php
@@ -10,17 +10,17 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200925084128 extends AbstractMigration
+final class Version20210224170718 extends AbstractMigration
 {
     public function getDescription() : string
     {
-        return 'Create events table';
+        return '';
     }
 
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE events (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, location VARCHAR(255) NOT NULL, price NUMERIC(5, 2) DEFAULT NULL, description LONGTEXT NOT NULL, starts_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE events (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL, location VARCHAR(255) NOT NULL, price NUMERIC(5, 2) DEFAULT NULL, description CLOB NOT NULL, starts_at DATETIME NOT NULL)');
     }
 
     public function down(Schema $schema) : void
